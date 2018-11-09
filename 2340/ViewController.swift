fad15c0ba0fb9a758d818bf8c72e3b7d3fcdaa56
Chengkai Yang <jenson.yang20@gmail.com>
2018-11-09 10:42:05 -0500

    Initial Commit

---
+++ 2340/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2340
+//
+//  Created by Chengkai Yang on 11/9/18.
+//  Copyright © 2018 Chengkai Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
